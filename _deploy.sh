5f03742a17a5b7fc433d49c26c782ad4fa7ae201
Emma Tosch <etosch@cs.umass.edu>
2020-10-13 20:55:52 -0400

    first movie with dame judy dench i guess

--- _deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eu
-export JEKYLL_VERSION=3.8
-docker run --rm \
-  --volume="$PWD:/srv/jekyll" \
-  -it jekyll/minimal:$JEKYLL_VERSION \
-  jekyll build
-rsync -r --progress _site/* root@jjfoley.me:/var/www/html/
